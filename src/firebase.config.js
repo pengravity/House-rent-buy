8d16606654e6d0c976406ba5323a7d9352c5c649
Andy <alipinski@mail.com>
2022-09-23 18:38:06 +0200

    adding needed files

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBpo3tMh2Ylrg7wL98hfBcROwwuvZHnN0w',
+  authDomain: 'house-rent-buy-23f8e.firebaseapp.com',
+  projectId: 'house-rent-buy-23f8e',
+  storageBucket: 'house-rent-buy-23f8e.appspot.com',
+  messagingSenderId: '1029167873011',
+  appId: '1:1029167873011:web:f9bd9ec4c528684bd4f086',
+  measurementId: 'G-KFVSGB1PSG',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
