821a4a3963c3c72443421ea3c00470f56d55f0c7
Andy <alipinski@mail.com>
2022-09-25 17:55:02 +0200

    added Routes to pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+
 function App() {
-  return <div>My app</div>;
+  return;
+
+  <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Explore />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+      </Routes>
+    </Router>
+  </>;
 }
 
 export default App;
