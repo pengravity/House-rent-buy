01dc8891b69d4bf42bafe8aab4a2133e4a80b0e8
Andy <alipinski@mail.com>
2022-09-26 14:01:17 +0200

    added signin  functionality and displaying name of the signed user

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -22,6 +24,25 @@ function Signin() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredentail = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredentail.user) {
+        navigate('/');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className='pageContainer'>
@@ -29,7 +50,7 @@ function Signin() {
           <p className='pageHeader'>Welcome Back</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type='email'
             className='emailInput'
