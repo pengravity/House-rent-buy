8d7e4f80995f0b616622e13be04cec2c2777555a
Andy <alipinski@mail.com>
2022-09-27 13:09:46 +0200

    fixing a typo

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthStatus } from '../hooks/useAuthStatus';
 
 const PrivateRoute = () => {
-  const { loggedIn, checkingStatus } = useAuthStatus;
+  const { loggedIn, checkingStatus } = useAuthStatus();
 
   if (checkingStatus) {
     return <h3>Loading...</h3>;
