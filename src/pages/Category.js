420a4de8240a15469a643d2e0c3a70f8129e8f9d
Andy <alipinski@mail.com>
2022-09-28 10:40:42 +0200

    added listingItem page

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -11,6 +11,7 @@ import {
 } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
+import ListingItem from '../components/ListingItem';
 
 function Category() {
   const [listings, setListings] = useState(null);
@@ -70,7 +71,11 @@ function Category() {
           <main>
             <ul className='categoryListings'>
               {listings.map((listing) => (
-                <h3 key={listing.id}>{listing.data.name}</h3>
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
               ))}
             </ul>
           </main>
