ad9795d802448442158938424cf6f1a8631a02ee
Andy <alipinski@mail.com>
2022-10-01 17:54:31 +0200

    functionality to edit listings

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -102,6 +102,8 @@ function Profile() {
     }
   };
 
+  const onEdit = (listingId) => navigate(`/edit-listing/${listingId}`);
+
   return (
     <div className='profile'>
       <header className='profileHeader'>
@@ -161,6 +163,7 @@ function Profile() {
                   id={listing.id}
                   listing={listing.data}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
